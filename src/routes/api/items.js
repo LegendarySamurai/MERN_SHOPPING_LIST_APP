f199c21194df10b04834c1c8d21d51a4da6be55b
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-02-28 19:10:54 +0200

    Created delete route with '/:id' path

--- src/routes/api/items.js
+++ src/routes/api/items.js
@@ -25,9 +25,8 @@ router.get('/', async (req, res) => {
 });
 
 
-
 // @route:        POST api/items
-// @description:  Create a Post
+// @description:  Create an item
 // @access:       Public
 
 /*
@@ -52,5 +51,30 @@ router.post('/', async (req, res) => {
 		console.log(err);
 	}
 });
+
+
+// @route:        DELETE api/items/:id
+// @description:  Delete an item
+// @access:       Public
+
+/*
+router.delete('/:id', (req, res) => {
+	Item.findById(req.params.id)
+		.then(item => item.remove().then(() => res.send({ success: 'Item removed' })))
+		.catch(err => res.status(404).send({ failure: 'Item with the indicated id does not exist' }));
+});
+*/
+
+router.delete('/:id', async (req, res) => {
+	try {
+		const item = await Item.findById(req.params.id);
+		await item.remove();
+		res.send({ success: 'Item removed' })
+	}
+	catch (err) {
+		res.status(404).send({ failure: 'Item with the indicated id does not exist' });
+	}
+});
+
 
 module.exports = router;
