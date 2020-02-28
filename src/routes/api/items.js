920757b53c26da1170031a3fc52cceb9d54593a5
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-02-28 18:55:59 +0200

    Created post route with '/' path

--- src/routes/api/items.js
+++ src/routes/api/items.js
@@ -7,15 +7,50 @@ const Item = require('../../models/item.model');
 // @route:        GET api/items
 // @description:  Get all items
 // @access:       Public
-// router.get('/', (req, res) => {
-// 	console.log('router.get() is working...');
-// 	Item.find()
-// 		.then(items => res.send(items))
-// });
-
+/*
 router.get('/', (req, res) => {
 	Item.find()
-		.then(items => res.json(items));
+		.then(items => res.send(items))
+});
+*/
+
+router.get('/', async (req, res) => {
+	try {
+		const items = await Item.find();
+		res.send(items);
+	}
+	catch (err) {
+		console.log(err);
+	}
+});
+
+
+
+// @route:        POST api/items
+// @description:  Create a Post
+// @access:       Public
+
+/*
+router.post('/', (req, res) => {
+	const item = new Item({
+		name: req.body.name
+	});
+
+	item.save()
+		.then(item => res.send(item));
+});
+*/
+
+router.post('/', async (req, res) => {
+	const item = new Item(req.body);
+
+	try {
+		await item.save();
+		res.send(item);
+	}
+	catch (err) {
+		console.log(err);
+	}
 });
 
 module.exports = router;
